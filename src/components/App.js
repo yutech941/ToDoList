405bd7a640e5ae531f59daff9b142eef863eeda3
小林優一 <y.k94cofitec179@gmail.com>
2020-04-24 15:05:19 +0900

    Container

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import SimpleList from "../material-ui/listUi";
+import BasicTextFields from "../material-ui/inputUi";
+import SimpleContainer from "../material-ui/centerUi";
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
+        <SimpleContainer />
+        <BasicTextFields />
         <SimpleList />
       </div>
     );
