ed0f6f9e4516c527c703e6aecc02dafb0ab62a47
小林優一 <y.k94cofitec179@gmail.com>
2020-04-24 13:25:06 +0900

    マテリアルUI-リスト

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SimpleList from "../material-ui/listUi";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <SimpleList />
+      </div>
+    );
+  }
+}
