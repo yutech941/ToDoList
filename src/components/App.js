4c5125ec12cb999a97bfc687bfe441b5324f4432
小林優一 <y.k94cofitec179@gmail.com>
2020-04-27 13:31:19 +0900

    ToDoCreator完成

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,37 @@
 import React from "react";
 import ButtonAppBar from "./material-ui/ButtonAppBar";
-import BasicTextField from "./material-ui/BasicTextField";
+import TodoCreator from "./material-ui/TodoCreator";
 import SimpleList from "./material-ui/SimpleList";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      listdata: [
+        {
+          id: this.createHashId(),
+          text: "sample todo1",
+        },
+      ],
+    };
+
+    this.callBackAddTask = this.callBackAddTask.bind(this);
+  }
+
+  //リストのキー生成
+  createHashId() {
+    return Math.random().toString(36).slice(-16);
+  }
+
+  //タスク追加
+  callBackAddTask(val) {
+    let nextData = this.state.listdata;
+    nextData.push({ id: this.createHashId(), text: val });
+    this.setState({
+      listdata: nextData,
+    });
+  }
+
   render() {
     return (
       <div>
@@ -14,7 +42,8 @@ export default class App extends React.Component {
             margin: "100px 500px 0 500px",
           }}
         >
-          <BasicTextField />
+          <TodoCreator callBackAddTask={this.callBackAddTask} />
+
           <SimpleList />
         </div>
       </div>
--- src/components/material-ui/SimpleList.js
+++ src/components/material-ui/SimpleList.js
@@ -2,11 +2,8 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
-import InboxIcon from "@material-ui/icons/Inbox";
-import DraftsIcon from "@material-ui/icons/Drafts";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,15 +24,9 @@ export default function SimpleList() {
     <div className={classes.root}>
       <List component="nav" aria-label="main mailbox folders">
         <ListItem button>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
           <ListItemText primary="Inbox" />
         </ListItem>
         <ListItem button>
-          <ListItemIcon>
-            <DraftsIcon />
-          </ListItemIcon>
           <ListItemText primary="Drafts" />
         </ListItem>
       </List>
