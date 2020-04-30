e8f2edb5f5a2ce5d8124bf7c8bdf9c810d68cb0f
小林優一 <y.k94cofitec179@gmail.com>
2020-04-30 14:09:36 +0900

    入力フォーム作成中

--- src/components/TodoCreator.js
+++ src/components/TodoCreator.js
@@ -18,46 +18,72 @@ export default class TodoCreator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      val: "",
+      titleVal: "",
+      contentVal: "",
     };
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    this.handleChangeContent = this.handleChangeContent.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeTitle(e) {
+    this.setState({
+      titleVal: e.target.value,
+    });
+  }
+  handleChangeContent(e) {
+    this.setState({
+      contentVal: e.target.value,
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
   }
 
   render() {
     return (
       <div>
-        <div style={styles.flexDirection}>
-          {/*日付表示*/}
-          <TimeCreator />
+        <form>
+          <div style={styles.flexDirection}>
+            {/*日付表示*/}
+            <TimeCreator />
 
-          {/*タイトル入力フォーム*/}
+            {/*タイトル入力フォーム*/}
+            <TextField
+              id="outlined-basic"
+              label="title"
+              variant="outlined"
+              value={this.state.titleVal}
+              onChange={this.handleChangeTitle}
+            />
+          </div>
+          {/*コンテンツ入力フォーム*/}
           <TextField
-            id="outlined-basic"
-            label="title"
+            id="outlined-full-width"
+            label="Label"
+            style={{ margin: 8 }}
+            placeholder="Placeholder"
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
             variant="outlined"
-            value={this.state.val}
-            onChange={this.handleChange}
-            onKeyUp={this.handleKeyUp}
+            value={this.state.contentVal}
+            onChange={this.handleChangeContent}
           />
-        </div>
-        {/*コンテンツ入力フォーム*/}
-        <TextField
-          id="outlined-full-width"
-          label="Label"
-          style={{ margin: 8 }}
-          placeholder="Placeholder"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="outlined"
-        />
 
-        <div style={styles.submitButton}>
-          <Button variant="contained" type="submit">
-            Submit
-          </Button>
-        </div>
+          <div style={styles.submitButton}>
+            <Button
+              variant="contained"
+              type="submit"
+              onSubmit={this.handleSubmit}
+            >
+              Submit
+            </Button>
+          </div>
+        </form>
       </div>
     );
   }
